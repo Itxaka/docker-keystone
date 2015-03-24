9736229988163ef4527db3b96a60d08974762337
Itxaka <itxakaserrano@gmail.com>
2015-03-24 13:24:54 +0100

    Setup the nova service so we can modify the projects and have quotas available

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM ubuntu:14.04
 MAINTAINER Itxaka Serrano Garcia <itxakaserrano@gmail.com>
-RUN apt-get update && apt-get install ubuntu-cloud-keyring -y && echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu trusty-updates/juno main" > /etc/apt/sources.list.d/cloudarchive-juno.list && apt-get update && apt-get install keystone python-keystoneclient -y
+RUN apt-get update && apt-get install ubuntu-cloud-keyring -y && echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu trusty-updates/juno main" > /etc/apt/sources.list.d/cloudarchive-juno.list && apt-get update && apt-get install keystone python-keystoneclient nova-api nova-cert nova-conductor nova-consoleauth \
+  nova-novncproxy nova-scheduler python-novaclient -y
 COPY keystone.conf /etc/keystone/keystone.conf
+COPY nova.conf /etc/nova/nova.conf
 RUN keystone-manage db_sync
 COPY run.sh /run.sh
 RUN chmod +x /run.sh
--- run.sh
+++ run.sh
@@ -13,9 +13,14 @@ export OS_SERVICE_ENDPOINT=http://127.0.0.1:35357/v2.0
 keystone tenant-create --name admin --description "Admin Tenant"
 keystone user-create --name admin --pass admin --email admin@admin.com
 keystone role-create --name admin
+keystone role-create --name _member_
 keystone user-role-add --user admin --tenant admin --role admin
 keystone tenant-create --name service --description "Service Tenant"
 keystone service-create --name keystone --type identity --description "OpenStack Identity"
+keystone user-create --name nova --pass nova
+keystone user-role-add --user nova --tenant service --role admin
+keystone service-create --name nova --type compute --description "OpenStack Compute"
+
 
 keystone endpoint-create \
   --service-id $(keystone service-list | awk '/ identity / {print $2}') \
@@ -24,7 +29,17 @@ keystone endpoint-create \
   --adminurl http://$EXTERNAL_IP:35357/v2.0 \
   --region regionOne
 
+keystone endpoint-create \
+  --service-id $(keystone service-list | awk '/ compute / {print $2}') \
+  --publicurl http://$EXTERNAL_IP:8774/v2/%\(tenant_id\)s \
+  --internalurl http://$EXTERNAL_IP:8774/v2/%\(tenant_id\)s \
+  --adminurl http://$EXTERNAL_IP:8774/v2/%\(tenant_id\)s \
+  --region regionOne
+
+
+nova-manage db sync
 
 kill $KEYSTONE_PID
 
-keystone-all
+keystone-all &
+nova-api
