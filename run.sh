b9450ab6d208b4a92c13b8ae1c511d72d878e8fb
Itxaka <itxakaserrano@gmail.com>
2015-09-11 14:36:37 +0200

    Revert "Add glance service to the container"
    This reverts commit b1c09f03ee8d863020a7b87bb3b7dfa25724f994.

--- run.sh
+++ run.sh
@@ -14,18 +14,12 @@ keystone tenant-create --name admin --description "Admin Tenant"
 keystone user-create --name admin --pass admin --email admin@admin.com
 keystone role-create --name admin
 keystone role-create --name _member_
-
 keystone user-role-add --user admin --tenant admin --role admin
 keystone tenant-create --name service --description "Service Tenant"
 keystone service-create --name keystone --type identity --description "OpenStack Identity"
-
 keystone user-create --name nova --pass nova
 keystone user-role-add --user nova --tenant service --role admin
 keystone service-create --name nova --type compute --description "OpenStack Compute"
-
-keystone user-create --name glance --pass glance
-keystone user-role-add --user glance --tenant service --role admin
-keystone service-create --name glance --type image --description "OpenStack Glance"
 
 
 keystone endpoint-create \
@@ -42,19 +36,10 @@ keystone endpoint-create \
   --adminurl http://$EXTERNAL_IP:8774/v2/%\(tenant_id\)s \
   --region regionOne
 
-keystone endpoint-create \
-  --service-id $(keystone service-list | awk '/ image / {print $2}') \
-  --publicurl http://$EXTERNAL_IP:9292 \
-  --internalurl http://$EXTERNAL_IP:9292 \
-  --adminurl http://$EXTERNAL_IP:9292 \
-  --region regionOne
 
 nova-manage db sync
-glance-manage db_sync
 
 kill $KEYSTONE_PID
 
 keystone-all &
-glance-registry &
-glance-api &
 nova-api
